165e31e4619fb97e2678b552d6fe56251dd51d71
sebastian.br <sebastian.br@web.de>
2024-04-17 01:36:39 +0200

    Improved performance using Paralell.ForEach

--- Charty/Chart/Analysis/GrowthVolatilityAnalyses/GrowthVolatilityAnalysis.cs
+++ Charty/Chart/Analysis/GrowthVolatilityAnalyses/GrowthVolatilityAnalysis.cs
@@ -4,6 +4,7 @@ using ScottPlot;
 using ScottPlot.Plottables;
 using ScottPlot.TickGenerators.TimeUnits;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -60,6 +61,8 @@ namespace Charty.Chart.ChartAnalysis.GrowthVolatilityAnalysis
                     break; // break out of loop because no fwdDataPoint was found
                 }
             }
+
+            AdjustmentPercentagePA = 7.5; // todo: set this to the federal interest rate + a risk rate that has to be calculated somehow
         }
 
 
@@ -70,6 +73,8 @@ namespace Charty.Chart.ChartAnalysis.GrowthVolatilityAnalysis
         double LowestMinimumPercentage { get; set; }
         double HighestMinimumPercentage { get; set; }
         string SaveDirectory { get; set; }
+
+        double AdjustmentPercentagePA { get; set; }
 
         /// <summary>
         /// To get the historic likelihood in % of a -x% drop in the TimePeriod used to initialize this class,
@@ -92,19 +97,45 @@ namespace Charty.Chart.ChartAnalysis.GrowthVolatilityAnalysis
             return historicLikelihood;
         }
 
-        private double GetHistoricLikelihoodOf_KO_Leverage_Success(double leverage)
+        static int GetExactDaysDifference(DateOnly startDate, DateOnly endDate)
         {
-            double percentageLossForKnockout = - ((1.0 / leverage) * 100.0);
-            int successCount = 0;
-            foreach(GrowthVolatilityAnalysisSubresult subResult in Subresults)
+            int daysDifference = 0;
+
+            int startYear = startDate.Year;
+            int endYear = endDate.Year;
+
+            for (int year = startYear; year < endYear; year++)
             {
-                if(percentageLossForKnockout < subResult.LowestPricePercent) // KO-barrier has not been touched
+                daysDifference += DateTime.IsLeapYear(year) ? 366 : 365;
+            }
+
+            daysDifference += endDate.DayOfYear - startDate.DayOfYear;
+            return daysDifference;
+        }
+
+        double GetAdjustedKObarrier(double koBarrier, DateOnly startDate, DateOnly endDate)
+        {
+            int numberOfDays = GetExactDaysDifference(startDate, endDate);
+            double effectiveAdjustmentPercentage = (AdjustmentPercentagePA / 365.0) * numberOfDays;
+            return Math.Pow(koBarrier, 1.0 + effectiveAdjustmentPercentage / 100.0);
+        }
+
+        bool WasBarrierHit(double initialBarrier, GrowthVolatilityAnalysisSubresult timeFrame)
+        {
+            SymbolDataPoint[] dataPoints = Symbol.DataPoints.Where(x => x.Date <= timeFrame.FwdDataPoint.Date && x.Date >= timeFrame.StartDataPoint.Date).ToArray();
+            DateOnly initialDate = dataPoints[0].Date;
+
+            foreach(SymbolDataPoint dataPoint in dataPoints)
+            {
+                double adjustedBarrier = GetAdjustedKObarrier(initialBarrier, initialDate, dataPoint.Date);
+
+                if(dataPoint.LowPrice <= adjustedBarrier)
                 {
-                    successCount++;
+                    return true;
                 }
             }
 
-            return successCount / (double)Subresults.Count;
+            return false;
         }
 
         /// <summary>
@@ -116,21 +147,20 @@ namespace Charty.Chart.ChartAnalysis.GrowthVolatilityAnalysis
         /// <returns>RETURN TYPE MIGHT NEED A SLIGHT REWORK!!!!</returns>
         private (double, double, double) GetHistoric_KO_Leverage_Overperformance_AndKOchance_AndKOorLossChance(double leverage)
         {
-            double percentageLossForKnockout = -((1.0 / leverage) * 100.0);
+            double initialKOfraction = (1.0 - (1.0 / leverage));
+            // assuming this is a LONG/CALL product
             List<double> leveragedOutcomes = new(); // elements in this list can take values typically ranging from 0 to e.g. 2.0, if the max leveraged return was 2.0x (+100%)
             int koCount = 0;
             int lossCount = 0;
 
             foreach (GrowthVolatilityAnalysisSubresult subResult in Subresults)
             {
-                if (percentageLossForKnockout < subResult.LowestPricePercent) // KO barrier has not been touched
+                double initialKObarrier = initialKOfraction * subResult.StartDataPoint.HighPrice; // pessimistic approach - assuming the asset was bought at the worst moment intra-day
+                if (!WasBarrierHit(initialKObarrier, subResult)) // KO barrier has not been touched
                 {
                     double leveragedOutcome = 1.0 + (leverage * subResult.GrowthPercent) / 100.0; // e.g. 1.4 for leverage := 2
                     double nonLeveragedOutcome = 1.0 + subResult.GrowthPercent / 100.0; // e.g. 1.2
                     double overPerformance = leveragedOutcome / nonLeveragedOutcome; // e.g. 1.1666 (16.6% relative overperformance compared to non-leveraged outcome)
-                    // leveragedOutcome = -0.009
-                    // subResult.LowestPricePercent = -32.358
-                    // subResult.GrowthPercent = -36.035
                     leveragedOutcomes.Add(overPerformance);
                     if(leveragedOutcome < 1.0)
                     {
@@ -198,6 +228,7 @@ namespace Charty.Chart.ChartAnalysis.GrowthVolatilityAnalysis
             {
                 DrawMinPercentGraph();
                 DrawLeveragedOverperformanceGraph();
+                DrawGrowthAnalysis();
             }
         }
 
@@ -226,45 +257,52 @@ namespace Charty.Chart.ChartAnalysis.GrowthVolatilityAnalysis
             myPlot.SavePng(SaveDirectory + Symbol.Overview.Symbol + "_MINPERCENT_GVA" + (int)TimePeriod + "month.png", numberOfBars * 30, 600);
         }
 
+        /// <summary>
+        /// Draws the historic overperformance of open-end LONG/CALL knockout certificates of specific leverages.
+        /// Considers the barrier being adjusted daily.
+        /// </summary>
         private void DrawLeveragedOverperformanceGraph()
         {
             ScottPlot.Plot myPlot = new();
-            myPlot.Title(Symbol.Overview.ToString() + " Leveraged Overperformance Analysis");
+            myPlot.Title(Symbol.Overview.ToString() + " Leveraged Overperformance Analysis over " + (int)TimePeriod + " months");
 
             double stepSize = 0.1; // do not set this below 0.1. If you do, adjust the barX = Math.Round... bit below.
-            double minLeverage = 1.3;
-            double maxLeverage = 10.0;
+            double minLeverage = 1.1;
+            double maxLeverage = 7.0;
             int numberOfBars = (int) double.Round((maxLeverage - minLeverage) / stepSize);
             List<ScottPlot.Bar> bars = new();
             List<Tick> tickList = new();
 
-            for (int i = 0; i <= numberOfBars; i++)
+            Parallel.ForEach(Partitioner.Create(0, numberOfBars + 1), range =>
             {
-                double barX = i * stepSize + minLeverage; // barX is the leverage and the actual intended x value of that bar, NOT the position of the bar on the graph
-                barX = Math.Round(barX, 1);
+                for (int i = range.Item1; i < range.Item2; i++)
+                {
+                    double barX = i * stepSize + minLeverage; // barX is the leverage and the actual intended x value of that bar, NOT the position of the bar on the graph
+                    barX = Math.Round(barX, 1);
 
-                double barPositionX = i;
-                (double, double, double) OverPerformanceAndKoChance = GetHistoric_KO_Leverage_Overperformance_AndKOchance_AndKOorLossChance(barX);
-                ScottPlot.Bar myBar = new ScottPlot.Bar() { Position = barPositionX, FillColor = Colors.Azure, Value = OverPerformanceAndKoChance.Item1};
-                tickList.Add(
-                    new(barPositionX, barX.ToString())
-                    );
-                myBar.Label = myBar.Value.Round(1).ToString() 
-                    + "\n" + OverPerformanceAndKoChance.Item2.Round(1).ToString() 
-                    + "\n" + OverPerformanceAndKoChance.Item3.Round(1).ToString();
+                    double barPositionX = i;
+                    (double, double, double) OverPerformanceAndKoChance = GetHistoric_KO_Leverage_Overperformance_AndKOchance_AndKOorLossChance(barX);
+                    ScottPlot.Bar myBar = new ScottPlot.Bar() { Position = barPositionX, FillColor = Colors.Azure, Value = OverPerformanceAndKoChance.Item1 };
+                    tickList.Add(
+                        new(barPositionX, barX.ToString())
+                        );
+                    myBar.Label = myBar.Value.Round(1).ToString()
+                        + "\n" + OverPerformanceAndKoChance.Item2.Round(1).ToString()
+                        + "\n" + OverPerformanceAndKoChance.Item3.Round(1).ToString();
 
-                myBar.LabelOffset = 25f;
-                myBar.FillColor = ScottPlot.Color.FromHSL(218, 92, 32, 0.7f);
-                bars.Add(myBar);
+                    myBar.LabelOffset = 25f;
+                    myBar.FillColor = ScottPlot.Color.FromHSL(218, 92, 32, 0.7f);
+                    bars.Add(myBar);
 
-                var koChanceLine = myPlot.Add.Line(barPositionX, 0, barPositionX, OverPerformanceAndKoChance.Item2);
-                koChanceLine.LineColor = Colors.Red;
-                koChanceLine.LineWidth = 8.0f;
+                    var koChanceLine = myPlot.Add.Line(barPositionX, 0, barPositionX, OverPerformanceAndKoChance.Item2);
+                    koChanceLine.LineColor = Colors.Red;
+                    koChanceLine.LineWidth = 8.0f;
 
-                var koOrLossLine = myPlot.Add.Line(barPositionX + koChanceLine.LineWidth / 100.0, 0, barPositionX + koChanceLine.LineWidth / 100.0, OverPerformanceAndKoChance.Item3);
-                koOrLossLine.LineColor = Colors.Orange;
-                koOrLossLine.LineWidth = koChanceLine.LineWidth / 2.0f;
-            }
+                    var koOrLossLine = myPlot.Add.Line(barPositionX + koChanceLine.LineWidth / 100.0, 0, barPositionX + koChanceLine.LineWidth / 100.0, OverPerformanceAndKoChance.Item3);
+                    koOrLossLine.LineColor = Colors.Orange;
+                    koOrLossLine.LineWidth = koChanceLine.LineWidth / 2.0f;
+                }
+            });
 
             var barPlot = myPlot.Add.Bars(bars.ToArray());
             barPlot.ValueLabelStyle.FontSize = 10f;
@@ -278,5 +316,190 @@ namespace Charty.Chart.ChartAnalysis.GrowthVolatilityAnalysis
             myPlot.Axes.Left.Label.Text = "Overperformance vs underlying Asset [%]";
             myPlot.SavePng(SaveDirectory + Symbol.Overview.Symbol + "_LVROVPF_GVA" + (int)TimePeriod + "month.png", numberOfBars * 30, 800);
         }
+
+        private double Annualize(double percentage)
+        {
+            double fraction = 1.0 + percentage / 100.0;
+            double numberOfYears = ((int)TimePeriod) / 12.0;
+            return Math.Round((Math.Pow(fraction, 1.0 / numberOfYears) - 1.0) * 100.0, 12);
+        }
+
+        private double DeAnnualize(double percentage)
+        {
+            double fraction = 1.0 + percentage / 100.0;
+            double numberOfYears = ((int)TimePeriod) / 12.0;
+            return Math.Round((Math.Pow(fraction, numberOfYears) - 1.0) * 100.0, 12);
+        }
+
+        private double GetLikelihoodOfAnnualizedGrowthLessThanPercentage(double percentage)
+        {
+            double annualizedPercentage = DeAnnualize(percentage);
+            return GetLikelihoodOfGrowthLessThanPercentage(annualizedPercentage);
+        }
+
+        private double GetLikelihoodOfGrowthLessThanPercentage(double percentage)
+        {
+            int count = 0;
+            foreach(GrowthVolatilityAnalysisSubresult subresult in Subresults)
+            {
+                if(subresult.GrowthPercent < percentage)
+                {
+                    count++;
+                }
+            }
+
+            double result = 
+                ((double)count
+                /
+                (double)Subresults.Count) * 100.0;
+
+            return result;
+        }
+
+        private double GetLikelihoodOfAnnualizedGrowthGreaterThanPercentage(double percentage)
+        {
+            double annualizedPercentage = DeAnnualize(percentage);
+            return GetLikelihoodOfGrowthGreaterThanPercentage(annualizedPercentage);
+        }
+
+        private double GetLikelihoodOfGrowthGreaterThanPercentage(double percentage)
+        {
+            int count = 0;
+            foreach (GrowthVolatilityAnalysisSubresult subresult in Subresults)
+            {
+                if (subresult.GrowthPercent > percentage)
+                {
+                    count++;
+                }
+            }
+
+            double result =
+                ((double)count
+                /
+                (double)Subresults.Count) * 100.0;
+
+            return result;
+        }
+
+        private double GetLikelihoodOfAnnualizedGrowthGreaterThanOrEqualToAndLessThanPercentages(double lPercentage, double hPercentage)
+        {
+            double annualizedPercentageMin = DeAnnualize(lPercentage);
+            double annualizedPercentageMax = DeAnnualize(hPercentage);
+            return GetLikelihoodOfGrowthGreaterThanOrEqualToAndLessThanPercentages(annualizedPercentageMin, annualizedPercentageMax);
+        }
+
+        private double GetLikelihoodOfGrowthGreaterThanOrEqualToAndLessThanPercentages(double lowerPercentage, double higherPercentage)
+        {
+            int count = 0;
+            foreach (GrowthVolatilityAnalysisSubresult subresult in Subresults)
+            {
+                if (subresult.GrowthPercent >= lowerPercentage && subresult.GrowthPercent < higherPercentage)
+                {
+                    count++;
+                }
+            }
+
+            double result =
+                ((double)count
+                /
+                (double)Subresults.Count) * 100.0;
+
+            return result;
+        }
+
+        private void DrawGrowthAnalysis()
+        {
+            ScottPlot.Plot myPlot = new();
+            double averageGrowthInTimePeriod = Subresults.Select(x => x.GrowthPercent).Average();
+            myPlot.Title(Symbol.Overview.ToString() + " Annualized Growth Analysis"
+                + "\n Average Growth in " + (int)TimePeriod + " months: " + averageGrowthInTimePeriod.Round(2) + "% & Annualized Growth: " + Annualize(averageGrowthInTimePeriod).Round(2) + "%");
+            myPlot.Axes.Title.Label.OffsetY = -35;
+            double minimum = -30;
+            double maximum = 50;
+            double stepSize = 5.0;
+            int numberOfBars = (int)double.Round((maximum - minimum) / stepSize);
+            List<ScottPlot.Bar> bars = new();
+            List<Tick> tickList = new();
+
+            ScottPlot.Bar initialBar = new ScottPlot.Bar() { Position = -1, FillColor = Colors.Azure, Value = GetLikelihoodOfAnnualizedGrowthLessThanPercentage(minimum) };
+            tickList.Add(
+                new(-1, "<" + minimum)
+                );
+            initialBar.Label = initialBar.Value.Round(1).ToString();
+
+            initialBar.LabelOffset = 10f;
+            bars.Add(initialBar);
+
+            for (int i = 0; i < numberOfBars; i++)
+            {
+                double barX = i * stepSize + minimum; // barX is the leverage and the actual intended x value of that bar, NOT the position of the bar on the graph
+                barX = Math.Round(barX, 1);
+
+                double barPositionX = i;
+                ScottPlot.Bar myBar = new ScottPlot.Bar() 
+                { Position = barPositionX, FillColor = Colors.Azure, 
+                    Value = GetLikelihoodOfAnnualizedGrowthGreaterThanOrEqualToAndLessThanPercentages(barX, barX+stepSize) };
+                tickList.Add(
+                    new(barPositionX, barX + "<=%<" + (barX + stepSize))
+                    );
+                myBar.Label = myBar.Value.Round(1).ToString();
+
+                myBar.LabelOffset = 10f;
+                bars.Add(myBar);
+            }
+
+            ScottPlot.Bar lastBar = new ScottPlot.Bar() { Position = numberOfBars, FillColor = Colors.Azure, Value = GetLikelihoodOfAnnualizedGrowthGreaterThanPercentage(maximum) };
+            tickList.Add(
+                new(numberOfBars, ">" + maximum)
+                );
+            lastBar.Label = lastBar.Value.Round(1).ToString();
+
+            lastBar.LabelOffset = 10f;
+            bars.Add(lastBar);
+
+            var barPlot = myPlot.Add.Bars(bars.ToArray());
+            barPlot.ValueLabelStyle.FontSize = 10f;
+
+            //https://scottplot.net/cookbook/5.0/CustomizingTicks/RotatedTicksLongLabels/
+            myPlot.Axes.Bottom.TickGenerator = new ScottPlot.TickGenerators.NumericManual(tickList.ToArray());
+            myPlot.Axes.Bottom.TickLabelStyle.Rotation = 45;
+            myPlot.Axes.Bottom.TickLabelStyle.Alignment = Alignment.MiddleLeft;
+
+            float largestLabelWidth = 0;
+            foreach (Tick tick in tickList)
+            {
+                PixelSize size = myPlot.Axes.Bottom.TickLabelStyle.Measure(tick.Label);
+                largestLabelWidth = Math.Max(largestLabelWidth, size.Width);
+            }
+
+            // ensure axis panels do not get smaller than the largest label
+            myPlot.Axes.Bottom.MinimumSize = largestLabelWidth + 30;
+            myPlot.Axes.Right.MinimumSize = largestLabelWidth;
+
+            myPlot.Axes.Bottom.Label.Text = "Annualized Growth [%] over " + (int)TimePeriod + " month period";
+            myPlot.Axes.Left.Label.Text = "Likelihood of Growth [%]";
+            myPlot.SavePng(SaveDirectory + Symbol.Overview.Symbol + "_GrowthAnalysis_" + (int)TimePeriod + "month.png", numberOfBars * 45, 500);
+        }
+
+        /*
+         Q: Why does this result not match the CAGR for the total duration of the analysis?
+
+        A: This is not a bug. Consider this example.
+        t 1	    2	3	4	5
+        p 100	110	121	133	146
+
+        growth = 46%
+        cagr = 7.86%
+
+        sliding windows (2):
+        121 / 100 = 21%
+        133 / 110 = 20.91%
+        146 / 121 = 20.66%
+        Avg Sliding windows: 20.856%
+        Annualized Sliding Window: ~9.92%
+
+        Merely taking the average of sliding windows of any length will usually not yield the cagr,
+        and the cagr is not very predictive of YoY or 2YoY growth, etc., as that is not its purpose
+         */
     }
 }
